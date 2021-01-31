8d9dfef76c1309ded8c8719e1120433acaee39c2
nero-15 <ti7805@gmail.com>
2021-01-31 19:08:08 +0900

    practice write

--- packages-lesson/zip/zip.go
+++ packages-lesson/zip/zip.go
@@ -1,8 +1,9 @@
-// https://golang.org/pkg/archive/zip/#example_Reader
+// https://golang.org/pkg/archive/zip
 package main
 
 import (
 	"archive/zip"
+	"bytes"
 	"fmt"
 	"io"
 	"log"
@@ -10,6 +11,13 @@ import (
 )
 
 func main() {
+	// practice reader
+	// https://golang.org/pkg/archive/zip/#example_Reader
+	// reader()
+	writer()
+}
+
+func reader() {
 	// Open a zip archive for reading.
 	r, err := zip.OpenReader("./testdata/memo.zip")
 	if err != nil {
@@ -31,5 +39,38 @@ func main() {
 		}
 		rc.Close()
 		fmt.Println()
+	}
+}
+
+func writer() {
+	// Create a buffer to write our archive to.
+	buf := new(bytes.Buffer)
+
+	// Create a new zip archive.
+	w := zip.NewWriter(buf)
+
+	// Add some files to the archive.
+	var files = []struct {
+		Name, Body string
+	}{
+		{"readme.txt", "This archive contains some text files."},
+		{"gopher.txt", "Gopher names:\nGeorge\nGeoffrey\nGonzo"},
+		{"todo.txt", "Get animal handling licence.\nWrite more examples."},
+	}
+	for _, file := range files {
+		f, err := w.Create(file.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = f.Write([]byte(file.Body))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Make sure to check the error on Close.
+	err := w.Close()
+	if err != nil {
+		log.Fatal(err)
 	}
 }
