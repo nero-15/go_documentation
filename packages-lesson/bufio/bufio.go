f9b34df703882535a28e2edb2a1fd16eca1354d0
nero-15 <ti7805@gmail.com>
2021-02-01 22:11:42 +0900

    bufio words

--- packages-lesson/bufio/bufio.go
+++ packages-lesson/bufio/bufio.go
@@ -19,7 +19,27 @@ func main() {
 	//emptyFinalToken()
 
 	// https://golang.org/pkg/bufio/#example_Scanner_lines
-	lines()
+	// lines()
+
+	// https://golang.org/pkg/bufio/#example_Scanner_words
+	words()
+}
+
+func words() {
+	// An artificial input source.
+	const input = "Now is the winter of our discontent,\nMade glorious summer by this sun of York.\n"
+	scanner := bufio.NewScanner(strings.NewReader(input))
+	// Set the split function for the scanning operation.
+	scanner.Split(bufio.ScanWords)
+	// Count the words.
+	count := 0
+	for scanner.Scan() {
+		count++
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading input:", err)
+	}
+	fmt.Printf("%d\n", count)
 }
 
 func lines() {
