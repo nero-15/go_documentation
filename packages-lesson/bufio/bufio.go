e6f29dea6119fd3a0cd028954ffb744b1803a63a
nero-15 <ti7805@gmail.com>
2021-02-01 22:09:45 +0900

    bufio lines

--- packages-lesson/bufio/bufio.go
+++ packages-lesson/bufio/bufio.go
@@ -16,7 +16,20 @@ func main() {
 	//custom()
 
 	//https://golang.org/pkg/bufio/#example_Scanner_emptyFinalToken
-	emptyFinalToken()
+	//emptyFinalToken()
+
+	// https://golang.org/pkg/bufio/#example_Scanner_lines
+	lines()
+}
+
+func lines() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text()) // Println will add back the final '\n'
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
 }
 
 func emptyFinalToken() {
