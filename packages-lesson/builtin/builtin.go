c63101abe84120edf1e8c2320025746841d37d6d
nero-15 <ti7805@gmail.com>
2021-02-02 21:16:12 +0900

    builtin

---
+++ packages-lesson/builtin/builtin.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	// Package builtin provides documentation for Go's predeclared identifiers. The items documented here are not actually in package builtin but their descriptions here allow godoc to present documentation for the language's special identifiers.
+}
