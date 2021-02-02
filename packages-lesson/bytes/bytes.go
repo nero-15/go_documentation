7594772d626e4bd07ce951ab321e5f7cc109d5ee
nero-15 <ti7805@gmail.com>
2021-02-02 21:47:04 +0900

    bytes map

--- packages-lesson/bytes/bytes.go
+++ packages-lesson/bytes/bytes.go
@@ -12,7 +12,21 @@ func main() {
 	// fields()
 	// hasPrefix()
 	// index()
-	join()
+	// join()
+	practiceMap()
+}
+
+func practiceMap() {
+	rot13 := func(r rune) rune {
+		switch {
+		case r >= 'A' && r <= 'Z':
+			return 'A' + (r-'A'+13)%26
+		case r >= 'a' && r <= 'z':
+			return 'a' + (r-'a'+13)%26
+		}
+		return r
+	}
+	fmt.Printf("%s", bytes.Map(rot13, []byte("'Twas brillig and the slithy gopher...")))
 }
 
 func join() {
