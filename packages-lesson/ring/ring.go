45816f37016ebe1a428ada944eaf5cdef9090e02
nero-15 <ti7805@gmail.com>
2021-02-04 21:46:19 +0900

    ring link

--- packages-lesson/ring/ring.go
+++ packages-lesson/ring/ring.go
@@ -7,7 +7,38 @@ import (
 
 func main() {
 	// do()
-	len()
+	// len()
+	link()
+}
+
+func link() {
+	// Create two rings, r and s, of size 2
+	r := ring.New(2)
+	s := ring.New(2)
+
+	// Get the length of the ring
+	lr := r.Len()
+	ls := s.Len()
+
+	// Initialize r with 0s
+	for i := 0; i < lr; i++ {
+		r.Value = 0
+		r = r.Next()
+	}
+
+	// Initialize s with 1s
+	for j := 0; j < ls; j++ {
+		s.Value = 1
+		s = s.Next()
+	}
+
+	// Link ring r and ring s
+	rs := r.Link(s)
+
+	// Iterate through the combined ring and print its contents
+	rs.Do(func(p interface{}) {
+		fmt.Println(p.(int))
+	})
 }
 
 func len() {
