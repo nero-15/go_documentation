9afa3c5e8a6a956e41024805da331321f514169a
nero-15 <ti7805@gmail.com>
2021-02-04 21:50:01 +0900

    ring prev

--- packages-lesson/ring/ring.go
+++ packages-lesson/ring/ring.go
@@ -10,7 +10,28 @@ func main() {
 	// len()
 	// link()
 	// move()
-	next()
+	// next()
+	prev()
+}
+
+func prev() {
+	// Create a new ring of size 5
+	r := ring.New(5)
+
+	// Get the length of the ring
+	n := r.Len()
+
+	// Initialize the ring with some integer values
+	for i := 0; i < n; i++ {
+		r.Value = i
+		r = r.Next()
+	}
+
+	// Iterate through the ring backwards and print its contents
+	for j := 0; j < n; j++ {
+		r = r.Prev()
+		fmt.Println(r.Value)
+	}
 }
 
 func next() {
