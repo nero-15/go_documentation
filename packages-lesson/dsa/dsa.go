dd588160a335141b351c90d3b19c8ddb0a87990d
nero-15 <ti7805@gmail.com>
2021-02-05 21:48:53 +0900

    dsa

---
+++ packages-lesson/dsa/dsa.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	// Package dsa implements the Digital Signature Algorithm, as defined in FIPS 186-3.
+	// The DSA operations in this package are not implemented using constant-time algorithms.
+}
