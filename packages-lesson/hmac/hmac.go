c5cf041df8018eed03b1ee274654609a739aff15
nero-15 <ti7805@gmail.com>
2021-02-06 21:29:52 +0900

    hmac

---
+++ packages-lesson/hmac/hmac.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	// Package hmac implements the Keyed-Hash Message Authentication Code (HMAC) as defined in U.S. Federal Information Processing Standards Publication 198. An HMAC is a cryptographic hash that uses a key to sign a message. The receiver verifies the hash by recomputing it using the same key.
+}
