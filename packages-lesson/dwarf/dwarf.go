f8f3ec8a610cd5869756592562c5afeb081d6d5e
nero-15 <ti7805@gmail.com>
2021-02-07 10:18:00 +0900

    dwarf

---
+++ packages-lesson/dwarf/dwarf.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	// Package dwarf provides access to DWARF debugging information loaded from executable files, as defined in the DWARF 2.0 Standard at http://dwarfstd.org/doc/dwarf-2.0.0.pdf
+}
