6690eeca7c408b804f697d1fa94c65365872c467
nero-15 <ti7805@gmail.com>
2021-02-12 22:17:42 +0900

    color

---
+++ packages-lesson/color/color.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"image/color"
+)
+
+func main() {
+	fmt.Println(color.Black)
+	fmt.Println(color.White)
+	fmt.Println(color.Transparent)
+	fmt.Println(color.Opaque)
+}
