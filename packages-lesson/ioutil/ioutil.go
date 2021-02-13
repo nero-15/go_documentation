80f03c6a5d2d651a10b501cc399051af5f2be350
nero-15 <ti7805@gmail.com>
2021-02-13 10:50:59 +0900

    ioutil

---
+++ packages-lesson/ioutil/ioutil.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	// readAll()
+	// readDir()
+	// tempDir()
+	writeFile()
+}
+
+func writeFile() {
+	message := []byte("Hello, Gophers!")
+	err := ioutil.WriteFile("ioutil/testdata/hello", message, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func tempDir() {
+	content := []byte("temporary file's content")
+	dir, err := ioutil.TempDir("", "example")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer os.RemoveAll(dir) // clean up
+
+	tmpfn := filepath.Join(dir, "tmpfile")
+	if err := ioutil.WriteFile(tmpfn, content, 0666); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func readDir() {
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
+}
+
+func readAll() {
+	r := strings.NewReader("Go is a general-purpose language designed with systems programming in mind.")
+
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s", b)
+}
