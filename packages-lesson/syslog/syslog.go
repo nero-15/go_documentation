e35fbfe3838841b5d52e6f19a2848124e5d95727
nero-15 <ti7805@gmail.com>
2021-02-13 11:00:48 +0900

    syslog

---
+++ packages-lesson/syslog/syslog.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	// Package syslog provides a simple interface to the system log service. It can send messages to the syslog daemon using UNIX domain sockets, UDP or TCP.
+}
