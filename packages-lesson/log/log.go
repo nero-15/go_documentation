923e2fdedd00707c88b11f6d9eff856b05ef6133
nero-15 <ti7805@gmail.com>
2021-02-13 10:56:00 +0900

    log

---
+++ packages-lesson/log/log.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+)
+
+func main() {
+	// var (
+	// 	buf    bytes.Buffer
+	// 	logger = log.New(&buf, "logger: ", log.Lshortfile)
+	// )
+	//
+	// logger.Print("Hello, log file!")
+	//
+	// fmt.Print(&buf)
+	output()
+}
+
+func output() {
+	var (
+		buf    bytes.Buffer
+		logger = log.New(&buf, "INFO: ", log.Lshortfile)
+
+		infof = func(info string) {
+			logger.Output(2, info)
+		}
+	)
+
+	infof("Hello world")
+
+	fmt.Print(&buf)
+}
