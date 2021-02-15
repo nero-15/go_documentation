6965ee20474be2f6f96e63b1d157632f1bf0743a
nero-15 <ti7805@gmail.com>
2021-02-15 12:01:48 +0900

    syntax

---
+++ packages-lesson/syntax/syntax.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	// Package syntax parses regular expressions into parse trees and compiles parse trees into programs. Most clients of regular expressions will use the facilities of package regexp (such as Compile and Match) instead of this package.
+}
