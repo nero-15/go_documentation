32ae9890c37f79cb62a1a6d9bdf5d5bca7fddb25
nero-15 <ti7805@gmail.com>
2021-02-17 21:48:55 +0900

    quick

---
+++ packages-lesson/quick/quick.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	// Package quick implements utility functions to help with black box testing.
+
+	// The testing/quick package is frozen and is not accepting new features.
+}
