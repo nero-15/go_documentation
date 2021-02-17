201f91da3494c4c14e9f6eff01e4b951d0367dbb
nero-15 <ti7805@gmail.com>
2021-02-17 21:43:17 +0900

    testing

---
+++ packages-lesson/testing/testing.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"sort"
+	"testing"
+)
+
+func main() {
+	// This reports a custom benchmark metric relevant to a
+	// specific algorithm (in this case, sorting).
+	testing.Benchmark(func(b *testing.B) {
+		var compares int64
+		for i := 0; i < b.N; i++ {
+			s := []int{5, 4, 3, 2, 1}
+			sort.Slice(s, func(i, j int) bool {
+				compares++
+				return s[i] < s[j]
+			})
+		}
+		// This metric is per-operation, so divide by b.N and
+		// report it as a "/op" unit.
+		b.ReportMetric(float64(compares)/float64(b.N), "compares/op")
+	})
+}
