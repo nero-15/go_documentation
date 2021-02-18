3f2627c9327e3fb986d9b56cc03678c40bc170a6
nero-15 <ti7805@gmail.com>
2021-02-18 22:45:51 +0900

    tzdata

---
+++ packages-lesson/tzdata/tzdata.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	// Package tzdata provides an embedded copy of the timezone database. If this package is imported anywhere in the program, then if the time package cannot find tzdata files on the system, it will use this embedded information.
+}
