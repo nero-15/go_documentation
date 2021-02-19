20529eed20aea6a451624b254529e3a4138314f5
nero-15 <ti7805@gmail.com>
2021-02-19 21:21:40 +0900

    html

--- packages-lesson/html/html.go
+++ packages-lesson/html/html.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"html"
+)
+
 func main() {
-	// TODO
+	// escape()
+	unescape()
+}
+
+func unescape() {
+	const s = `&quot;Fran &amp; Freddie&#39;s Diner&quot; &lt;tasty@example.com&gt;`
+	fmt.Println(html.UnescapeString(s))
+}
+
+func escape() {
+	const s = `"Fran & Freddie's Diner" <tasty@example.com>`
+	fmt.Println(html.EscapeString(s))
 }
