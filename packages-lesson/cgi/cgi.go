a262a1575a97af36e919c7938149842a297c7c6c
nero-15 <ti7805@gmail.com>
2021-02-20 16:25:53 +0900

    cgi

---
+++ packages-lesson/cgi/cgi.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	// Package cgi implements CGI (Common Gateway Interface) as specified in RFC 3875.
+
+	// Note that using CGI means starting a new process to handle each request, which is typically less efficient than using a long-running server. This package is intended primarily for compatibility with existing systems.
+}
