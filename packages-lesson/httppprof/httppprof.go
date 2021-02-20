6aee2f2e12456737f27d5cf567fd306b5b436585
nero-15 <ti7805@gmail.com>
2021-02-20 21:10:50 +0900

    httppprof

---
+++ packages-lesson/httppprof/httppprof.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	// Package pprof serves via its HTTP server runtime profiling data in the format expected by the pprof visualization tool.
+	// The package is typically only imported for the side effect of registering its HTTP handlers. The handled paths all begin with /debug/pprof/.
+}
