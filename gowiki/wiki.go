d326fb687ed72aee4f097530a51f961cdd9f3def
nero-15 <ti7805@gmail.com>
2021-02-24 21:59:09 +0900

    template caching

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -59,16 +59,13 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, err := template.ParseFiles(tmpl + ".html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	err = t.Execute(w, p)
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 
 func main() {
 	http.HandleFunc("/view/", viewHandler)
